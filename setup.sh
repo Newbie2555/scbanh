e9ee69a414555f0db41a0a5fd38b74528ddce91c
Newbie2555 <107703349+Newbie2555@users.noreply.github.com>
2022-08-03 23:24:32 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -267,37 +267,37 @@ echo "1.2" > /home/ver
 echo " "
 echo "Installation has been completed!!"
 echo " "
-echo "=================================-AutoScript By KangTuru Project-===========================" | tee -a log-install.txt
+echo "=================================-AutoScript By KangTuru-===========================" | tee -a log-install.txt
 echo "" | tee -a log-install.txt
 echo "----------------------------------------------------------------------------" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
-echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
-echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 990"  | tee -a log-install.txt
-echo "   - Stunnel5                : 443, 445, 777"  | tee -a log-install.txt
-echo "   - Dropbear                : 109, 143 , 443"  | tee -a log-install.txt
-echo "   - Squid Proxy             : 3128, 8080"  | tee -a log-install.txt
-echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
-echo "   - Nginx                   : 89"  | tee -a log-install.txt
-echo "   - Wireguard               : 7070"  | tee -a log-install.txt
-echo "   - L2TP/IPSEC VPN          : 1701"  | tee -a log-install.txt
-echo "   - PPTP VPN                : 1732"  | tee -a log-install.txt
-echo "   - SSTP VPN                : 444"  | tee -a log-install.txt
-echo "   - Shadowsocks-R           : 1443-1543"  | tee -a log-install.txt
-echo "   - SS-OBFS TLS             : 2443-2543"  | tee -a log-install.txt
-echo "   - SS-OBFS HTTP            : 3443-3543"  | tee -a log-install.txt
-echo "   - XRAYS Vmess TLS         : 8443"  | tee -a log-install.txt
-echo "   - XRAYS Vmess None TLS    : 80"  | tee -a log-install.txt
-echo "   - XRAYS Vless TLS         : 8443"  | tee -a log-install.txt
-echo "   - XRAYS Vless None TLS    : 80"  | tee -a log-install.txt
-echo "   - XRAYS Trojan            : 2083"  | tee -a log-install.txt
-echo "   - Websocket TLS           : 443"  | tee -a log-install.txt
-echo "   - Websocket None TLS      : 8880"  | tee -a log-install.txt
-echo "   - Websocket Ovpn          : 2086"  | tee -a log-install.txt
-echo "   - OHP SSH                 : 8181"  | tee -a log-install.txt
-echo "   - OHP Dropbear            : 8282"  | tee -a log-install.txt
-echo "   - OHP OpenVPN             : 8383"  | tee -a log-install.txt
-echo "   - Tr Go                   : 2087"  | tee -a log-install.txt
+echo "   ➤ OpenSSH                 : 22"  | tee -a log-install.txt
+echo "   ➤ OpenVPN                 : TCP 1194, UDP 2200, SSL 990"  | tee -a log-install.txt
+echo "   ➤ Stunnel5                : 443, 445, 777"  | tee -a log-install.txt
+echo "   ➤ Dropbear                : 109, 143 , 443"  | tee -a log-install.txt
+echo "   ➤ Squid Proxy             : 3128, 8080"  | tee -a log-install.txt
+echo "   ➤ Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
+echo "   ➤ Nginx                   : 89"  | tee -a log-install.txt
+echo "   ➤ Wireguard               : 7070"  | tee -a log-install.txt
+echo "   ➤ L2TP/IPSEC VPN          : 1701"  | tee -a log-install.txt
+echo "   ➤ PPTP VPN                : 1732"  | tee -a log-install.txt
+echo "   ➤ SSTP VPN                : 444"  | tee -a log-install.txt
+echo "   ➤ Shadowsocks-R           : 1443-1543"  | tee -a log-install.txt
+echo "   ➤ SS-OBFS TLS             : 2443-2543"  | tee -a log-install.txt
+echo "   ➤ SS-OBFS HTTP            : 3443-3543"  | tee -a log-install.txt
+echo "   ➤ XRAYS Vmess TLS         : 8443"  | tee -a log-install.txt
+echo "   ➤ XRAYS Vmess None TLS    : 80"  | tee -a log-install.txt
+echo "   ➤ XRAYS Vless TLS         : 8443"  | tee -a log-install.txt
+echo "   ➤ XRAYS Vless None TLS    : 80"  | tee -a log-install.txt
+echo "   ➤ XRAYS Trojan            : 2083"  | tee -a log-install.txt
+echo "   ➤ Websocket TLS           : 443"  | tee -a log-install.txt
+echo "   ➤ Websocket None TLS      : 8880"  | tee -a log-install.txt
+echo "   ➤ Websocket Ovpn          : 2086"  | tee -a log-install.txt
+echo "   ➤ OHP SSH                 : 8181"  | tee -a log-install.txt
+echo "   ➤ OHP Dropbear            : 8282"  | tee -a log-install.txt
+echo "   ➤ OHP OpenVPN             : 8383"  | tee -a log-install.txt
+echo "   ➤ Tr Go                   : 2087"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
 echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
@@ -315,12 +315,12 @@ echo "   - White Label" | tee -a log-install.txt
 echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   - Dev/Main                : Horas Marolop Amsal Siregar"  | tee -a log-install.txt
-echo "   - Recode                  : Waan Store" | tee -a log-install.txt
+echo "   - Recode                  : KangTuru" | tee -a log-install.txt
 echo "   - Telegram                : ( Gak ada )"  | tee -a log-install.txt
 echo "   - Instagram               : ( Gak ada )"  | tee -a log-install.txt
 echo "   - Whatsapp                : ( Gak ada )"  | tee -a log-install.txt
 echo "   - Facebook                : ( Gak ada )" | tee -a log-install.txt
-echo "----------------------AutoScript By KangTuru Project----------------------" | tee -a log-install.txt
+echo "----------------------AutoScript By KangTuru----------------------" | tee -a log-install.txt
 echo ""
 echo " Reboot 15 Sec"
 sleep 15
